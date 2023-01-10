029bd306390752e4d010a2e84aa47a0c852f15b1
Abdelrahman.Ayman <79347475+abdo-ayman8642@users.noreply.github.com>
2023-01-10 23:44:19 +0200

    finishing home page and basic styling

--- little-lemon-app/src/App.js
+++ little-lemon-app/src/App.js
@@ -1,7 +1,41 @@
 import "./App.css";
+import logo from "./assets/6866595.png";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <main>
+      <header>
+        <nav>
+          <div className="restName">
+            <span>
+              Little Lemon
+              <img src={logo} alt="logo" className="logo" />
+            </span>
+            <span>Restaurant</span>
+          </div>
+          <ul className="navLinks">
+            <li>About</li>
+            <li>Reservation</li>
+            <li>Contact us</li>
+          </ul>
+          <div className="socialLinks">
+            <ul>
+              <li>
+                <FaFacebookF />
+              </li>
+              <li>
+                <FaTwitter />
+              </li>
+              <li>
+                <FaInstagram />
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </main>
+  );
 }
 
 export default App;
